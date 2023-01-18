be3304e63500d44d00421d00f6f995a40c4cd559
Prachi Waidande <78075627+waidandeprachi@users.noreply.github.com>
2023-01-18 18:45:08 +0530

    Add files via upload

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/mcs/currencyconversionservice/controller/currencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.mcs.currencyconversionservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
+public interface currencyExchangeProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
