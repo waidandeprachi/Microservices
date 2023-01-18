c6739cb40ba04239d14ad1e4d1d292bd6c8e1664
Prachi Waidande <78075627+waidandeprachi@users.noreply.github.com>
2023-01-18 18:46:59 +0530

    Add files via upload

---
+++ currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id, currency_from, currency_to, environment, conversion_multiple)
+values(1001, 'USD', 'INR', '', 65);
+
+insert into currency_exchange(id, currency_from, currency_to, environment, conversion_multiple)
+values(1002, 'USD', 'INR', '', 75);
+
+insert into currency_exchange(id, currency_from, currency_to, environment, conversion_multiple)
+values(1003, 'USD', 'INR', '', 25);
